e953b887cd514a659cc7ea1c61a58a6dfc67ade9
ROBERT SEARS <Robert.Sears@LibertyMutual.com>
2017-04-12 09:45:47 -0400

    Update status to stopped when count reaces zero

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -26,12 +26,30 @@ var Controls =  require('Controls');
                 }
             }
         },
+       // componentWillUpdate: function (nextProps, nextState) {  // these called lifecycle methods
+        //   // console.log('componentWillMount');
+      //  },
+      //  componentWillMount: function () {  // these called lifecycle methods
+      //      console.log('componentWillMount');
+     //   },
+     //   componentDidMount: function () {
+     //       console.log('componentDidMount');
+     //   },
+        componentWillUnmount: function () {
+            //console.log('componentDidUnmount');
+            clearInterval(this.timer);
+            this.timer = undefined;
+        },
         startTimer: function () {
             this.timer =  setInterval(() => {
                 var newCount = this.state.count -1;
                 this.setState({
                     count: newCount >= 0 ? newCount : 0
                 });
+
+                if (newCount === 0) {
+                    this.setState({countdownStatus: 'stopped'});
+                }
             }, 1000);
         },
         handleSetCountdown: function (seconds) { // this will get called when the form get valid input
